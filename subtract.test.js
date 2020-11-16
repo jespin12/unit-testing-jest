efdc8e67ee766e81ef56be170fe8d53399a27193
Jesse Espinoza <jespin12@uncc.edu>
2020-11-15 23:56:38 -0500

    Introduction to Testing in JS with Jest

---
+++ subtract.test.js
@@ -0,0 +1,8 @@
+const { TestScheduler } = require('jest')
+const subtract = require('./subtract')
+
+test('properly subtracting two numbers', () => {
+    expect(
+        subtract(1,2)
+    ).toBe(-1)
+})
